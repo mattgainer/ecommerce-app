1daf535de83e62520fc2e9a36618810010634108
Matt Gainer <matgainer@gmail.com>
2015-11-22 16:40:04 -0800

    Added devise gem, changed navbar to add dropdown.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'seed_dump'
 
 gem 'geocoder'
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'products#index'
+  devise_for :users
+  root to: 'products#index'
   
   get '/products', to: 'products#index'
 
