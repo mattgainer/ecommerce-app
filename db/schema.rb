ce3593f200fb7e0da538ef3558f207de8e335862
Matt Gainer <matgainer@gmail.com>
2015-11-23 21:29:51 -0800

    starting to add first and last name to user table and devise forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124044901) do
+ActiveRecord::Schema.define(version: 20151124045755) do
 
   create_table "images", force: :cascade do |t|
     t.text     "url",        limit: 65535
@@ -70,6 +70,8 @@ ActiveRecord::Schema.define(version: 20151124044901) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.integer  "role_id",                limit: 4,   default: 2
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
